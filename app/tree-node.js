5e7a5aca5f261d833e42ce9b245c0f35e16f3522
thefreshduke <scottyshaw1@gmail.com>
2015-02-07 16:30:19 +0000

    Updated CSS

--- app/tree-node.js
+++ app/tree-node.js
@@ -12,33 +12,49 @@ export default class TreeNodeComponent extends React.Component {
     var childProperties = [];
     if (this.props.node.properties != null) {
       _.forOwn(this.props.node.properties, (node, name) => {
-        childProperties.push(<li key={name}>{name}: <TreeNodeComponent node={node} /></li>);
+        childProperties.push(<li key={name}><TreeNodeComponent node={node} /></li>);
       });
     }
     var cx = React.addons.classSet;
 
     var expandIconClasses = cx({
       'glyphicon': true,
+      'expand-button': true,
       'glyphicon-minus': this.state.visible,
       'glyphicon-plus': !this.state.visible,
     });
 
     var childElementVisibility = cx({
+      'children-nodes': true,
       'children-visible': this.state.visible,
       'children-hidden': !this.state.visible,
     })
 
     return (
       <div>
-        <h5>
+        <div>
           <i onClick={this.toggle.bind(this)} className={expandIconClasses}></i>
-          <span>{this.props.node.title}</span>
-        </h5>
-        <p>Description: {this.props.node.description}</p>
-        <TypeComponent type={this.props.node.type}></TypeComponent>
-        <ul className={childElementVisibility}>
-          {childProperties}
-        </ul>
+        </div>
+        <div className="panel panel-default node-content">
+          <div className="panel-heading">{this.props.node.title}</div>
+          <table className="table">
+            <tr>
+              <td className="name">description</td>
+              <td className="val">{this.props.node.description}</td>
+              <td className="edit"><i className="glyphicon glyphicon-edit"></i></td>
+            </tr>
+            <tr>
+              <td className="name">type</td>
+              <td className="val">
+                <TypeComponent type={this.props.node.type}></TypeComponent>
+              </td>
+              <td className="edit"><i className="glyphicon glyphicon-edit"></i></td>
+            </tr>
+          </table>
+          <ul className={childElementVisibility}>
+            {childProperties}
+          </ul>
+        </div>
       </div>
     );
   }
