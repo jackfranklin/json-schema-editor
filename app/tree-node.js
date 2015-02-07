9e427922e3da386160942db7b8c870cffbc72b6f
Jack Franklin <jack@jackfranklin.net>
2015-02-07 13:54:30 +0000

    iain cannot indent a thing

--- app/tree-node.js
+++ app/tree-node.js
@@ -2,36 +2,38 @@ import React from 'react';
 
 export default class TreeNodeComponent extends React.Component {
   getInitialState() {
-  return {
-    visible: true,
-  };
-  }
-  render() {
-  var childProperties;
-  if (this.props.node.properties != null) {
-    childProperties = this.props.node.properties.map((node, indx) => {
-      return <li key={index}><TreeNodeComponent
-    })
+    return {
+      visible: true,
+    };
   }
 
-  var style = {};
-  if (!this.state.visible) {
-    style.display = "none";
+  render() {
+    var childProperties;
+    if (this.props.node.properties != null) {
+      childProperties = this.props.node.properties.map((node, indx) => {
+        return <li key={index}><TreeNodeComponent
+      })
+    }
+
+    var style = {};
+    if (!this.state.visible) {
+      style.display = "none";
+    }
+    return (
+      <div>
+        <h5 onClick={this.toggle}>
+          {this.props.node.title}
+        </h5>
+        <p>{this.props.node.description}</p>
+        <ul style={style}>
+          {childProperties}
+        </ul>
+      </div>
+    );
   }
-  return (
-    <div>
-      <h5 onClick={this.toggle}>
-        {this.props.node.title}
-      </h5>
-      <p>{this.props.node.description}</p>
-      <ul style={style}>
-        {childProperties}
-      </ul>
-    </div>
-  );
-  },
+
   toggle() {
-  this.setState({visible: !this.state.visible});
+    this.setState({visible: !this.state.visible});
   }
 }
 
